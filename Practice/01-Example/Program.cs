7e657c6731bfd20673a7227a152a7391821f8900
aravind <tnarwind@gmail.com>
2024-12-07 06:09:56 +0530

    added 2 new projects

--- Practice/01-Example/Program.cs
+++ Practice/01-Example/Program.cs
@@ -2,9 +2,78 @@
 {
     internal class Program
     {
+        static Program()
+        {
+            Console.WriteLine("Hello from program constructor");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Example example = new Example(2);
+            //Console.WriteLine(example.b);
+            Console.WriteLine(example.c);
+            Example.GetNumber();
+            UnderstandingTypesOfVariables understandingTypesOfVariables = new UnderstandingTypesOfVariables();
+            UnderstandingTypesOfVariables understandingTypesOfVariables2 = new UnderstandingTypesOfVariables();
+
+            var valueOfA = UnderstandingTypesOfVariables.a;
+            //understandingTypesOfVariables.e = 10;
+            //UnderstandingTypesOfVariables.d = 10;
+        }
+    }
+        
+    public class Example
+    {
+        static int a = 1;
+        private int b;
+        protected internal int c;
+        static Example()
+        {
+            Console.WriteLine("Static constructor of example class");
+        }
+        //private Example(string number)
+        //{
+        //    Console.WriteLine($"{number}" + a);
+        //}
+        public Example(int b)
+        {
+            //this.b = b;
+        }
+        public Example(string name)
+        {
+            //this.b = b;
+        }
+        public static void GetNumber()
+        {
+            Example example = new Example(1);
+            Console.WriteLine(example.b);
+        }
+    }
+    public static class StaticClass
+    {
+        static int a;
+    }
+    public class UnderstandingTypesOfVariables
+    {
+        public const int a = 1;
+        //static const int b = 2;
+        public readonly int c;
+        public static readonly int d = 5;
+        public readonly int e = 9;
+        public int m;
+
+        public UnderstandingTypesOfVariables()
+        {
+            this.e = 11;
+        }
+        static UnderstandingTypesOfVariables()
+        {
+            //m = 10;
+            d = 10;
+        }
+        public void PrintValues()
+        {
+            Console.WriteLine("Value of a is: " + a);
         }
     }
 }
---
+++ Practice/02-Example/Program.cs
@@ -0,0 +1,36 @@
+﻿using _01_Example;
+
+namespace _02_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            _01_Example.Example example = new _01_Example.Example(1);
+        }
+    }
+    public class Example
+    {
+
+    }
+    public class Example2 : _01_Example.Example
+    {
+        public Example2(string s) : base("hello")
+        {
+
+        }
+        public Example2(int a) : base(a)
+        {
+
+        }
+        public Example2(double d): base(4)
+        {
+
+        }
+        public void AccessTheProtectedVariable()
+        {
+            Console.WriteLine(c);
+        }
+    }
+}   
